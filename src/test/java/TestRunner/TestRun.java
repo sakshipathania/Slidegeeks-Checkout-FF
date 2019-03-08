6d29633797373a5d06cdc888a5eb58fe2fd03179
shadab36 <team.qanalyst@gmail.com>
2019-03-08 06:59:42 +0000

    test

--- src/test/java/TestRunner/TestRun.java
+++ src/test/java/TestRunner/TestRun.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"."},tags= {"@signupTest"},
+@CucumberOptions(features = {"."},
 		plugin ={
 				"pretty", "html:target/cucumber-html-report","json:target/cucumber.json", "usage:target/usage.jsonx", "junit:target/cucumber.xml"},
 		monochrome=true)
