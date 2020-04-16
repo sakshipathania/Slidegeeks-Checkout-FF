bd572de71bb61e3b8620f9166e50a9b89f8e12ef
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-04-16 16:30:37 +0530

    Update Email_Signin_paidUser_stepDefinition.java

--- src/test/java/TestRunner/loginSteps/Email_Signin_paidUser_stepDefinition.java
+++ src/test/java/TestRunner/loginSteps/Email_Signin_paidUser_stepDefinition.java
@@ -57,6 +57,7 @@ public class Email_Signin_paidUser_stepDefinition extends SetupClass {
          public void user_navigate_to_complete_deck_from_account_dashboard_page() throws InterruptedException  {
     
 	 driver.get("https://www.slideteam.net/complete-powerpoint-decks-presentations/all-powerpoint-complete-decks.html");
+	 wait.implictywait(driver);
 	 Thread.sleep(3000);
 	 
 	//wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("li.product:nth-child(1) > div:nth-child(1) > div:nth-child(2) > strong:nth-child(1) > span:nth-child(1) > a:nth-child(1)"))).click();
