ca6b8e95ac5935c991a7569fd700079d8a592311
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-04-16 14:23:07 +0530

    Update Forgotpassword.java

--- src/test/java/TestRunner/loginSteps/Forgotpassword.java
+++ src/test/java/TestRunner/loginSteps/Forgotpassword.java
@@ -57,7 +57,7 @@ import webApp.PerformAction;
 	 driver.get("https://www.slideteam.net/complete-powerpoint-decks-presentations/all-powerpoint-complete-decks.html");
 	 Thread.sleep(3000);
 	 
-	 WebElement select_product=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("li.product:nth-child(1) > div:nth-child(1) > div:nth-child(2) > strong:nth-child(1) > span:nth-child(1) > a:nth-child(1)")));
+	 WebElement select_product=driver.findElement(By.cssSelector("li.product:nth-child(1) > div:nth-child(1) > div:nth-child(2) > strong:nth-child(1) > span:nth-child(1) > a:nth-child(1)")));
 	  Thread.sleep(2000);   
 	 select_product.click();
 	    Thread.sleep(2000);
