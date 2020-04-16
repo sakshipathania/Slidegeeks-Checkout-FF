c02e8d239c047fbdf48e709bcbbf92f5364dde7c
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-04-16 17:51:15 +0530

    Update Email_Signin_paidUser_stepDefinition.java

--- src/test/java/TestRunner/loginSteps/Email_Signin_paidUser_stepDefinition.java
+++ src/test/java/TestRunner/loginSteps/Email_Signin_paidUser_stepDefinition.java
@@ -53,29 +53,6 @@ public class Email_Signin_paidUser_stepDefinition extends SetupClass {
 		}
 	}
 
-	/* @Then("^user goes to complete deck from account dashboard page$")
-         public void user_goes_to_complete_deck_from_account_dashboard_page() throws InterruptedException  {
-    
-	 //driver.get("https://www.slideteam.net/complete-powerpoint-decks-presentations/all-powerpoint-complete-decks.html");
-	 //wait.implictywait(driver);
-	 Thread.sleep(3000);
-	 
-	WebElement select_product = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("li.product:nth-child(1) > div:nth-child(1) > div:nth-child(2) > strong:nth-child(1) > span:nth-child(1) > a:nth-child(1)"))).click();
-          Thread.sleep(2000);   
-	//ac.moveToElement(select_product).build().perform();	 
-	 select_product.click();
-	   // Thread.sleep(2000);
-	    
-            } */
-
-	@Then("^Click on download this presentation link$")
-	public void click_on_Download_this_presentation_link() throws InterruptedException {
-		WebElement presentation= driver.findElement(By.cssSelector("#clicking"));
-		wait.implictywait(driver);
-		presentation.click();
-		wait.implictywait(driver);
-		Thread.sleep(8000);
-	}
 	@Then ("^chat window option\\.$")
 	public void close_chat_window() throws InterruptedException {
 		try {
@@ -147,7 +124,19 @@ public class Email_Signin_paidUser_stepDefinition extends SetupClass {
 
 		}
 	}
-
+        
+	@Then("^user navigate to complete deck from account dashboard page$")
+         public void user_navigate_to_complete_deck_from_account_dashboard_page() throws InterruptedException  {
+    
+	// driver.get("https://www.slideteam.net/complete-powerpoint-decks-presentations/all-powerpoint-complete-decks.html");
+	 Thread.sleep(3000);
+	 
+	 WebElement select_product= driver.findElement(By.cssSelector("li.product:nth-child(1) > div:nth-child(1) > div:nth-child(2) > strong:nth-child(1) > span:nth-child(1) > a:nth-child(1)")));
+	  Thread.sleep(2000);   
+	 select_product.click();
+	    Thread.sleep(2000);
+          }
+	
 	@Then("^the download this presenetion link is appeared$")
 	public void verify_uesr_validation_message_for_Email_Address() throws Throwable {
 
@@ -162,6 +151,14 @@ public class Email_Signin_paidUser_stepDefinition extends SetupClass {
 
 		}
 	}
-
+	
+        @Then("^Click on download this presentation link$")
+	public void click_on_Download_this_presentation_link() throws InterruptedException {
+		WebElement presentation= driver.findElement(By.cssSelector("#clicking"));
+		wait.implictywait(driver);
+		presentation.click();
+		wait.implictywait(driver);
+		Thread.sleep(8000);
+	}
 }
 
