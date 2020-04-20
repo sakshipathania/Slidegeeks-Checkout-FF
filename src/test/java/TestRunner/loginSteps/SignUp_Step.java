926e4f8900b7bf1ed2676314c6ca7abe19c37682
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-04-20 16:33:13 +0530

    Update SignUp_Step.java

--- src/test/java/TestRunner/loginSteps/SignUp_Step.java
+++ src/test/java/TestRunner/loginSteps/SignUp_Step.java
@@ -50,16 +50,16 @@ public class SignUp_Step extends SetupClass {
 		}
 	}
 
-       /* @Then("^user navigate to complete deck from account dashboard page$")
+        @Then("^user navigate to complete deck from account dashboard page$")
          public void user_navigate_to_complete_deck_from_account_dashboard_page() throws InterruptedException  {
     
 	 driver.get("https://www.slideteam.net/complete-powerpoint-decks-presentations/all-powerpoint-complete-decks.html");
 	 Thread.sleep(3000);
 	 
-	// WebElement select_product=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("li.product:nth-child(1) > div:nth-child(1) > div:nth-child(2) > strong:nth-child(1) > span:nth-child(1) > a:nth-child(1)")));
-	  //Thread.sleep(2000);   
-	 //select_product.click();
-	   // Thread.sleep(2000);
+	 WebElement select_product=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("li.product:nth-child(1) > div:nth-child(1) > div:nth-child(2) > strong:nth-child(1) > span:nth-child(1) > a:nth-child(1)"));
+	  Thread.sleep(2000);   
+	 select_product.click();
+	  Thread.sleep(2000);
 	    
             }
 
