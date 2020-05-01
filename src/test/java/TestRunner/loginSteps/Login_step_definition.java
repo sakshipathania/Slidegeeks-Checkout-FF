dc346afbffd999e91b6875ef856c837cc7a5d839
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-05-01 18:33:16 +0530

    Update Login_step_definition.java

--- src/test/java/TestRunner/loginSteps/Login_step_definition.java
+++ src/test/java/TestRunner/loginSteps/Login_step_definition.java
@@ -259,14 +259,14 @@ public class Login_step_definition extends SetupClass {
 	@Then("^See default payment option as paypal\\.$")
 	public void see_default_payment_option_as_paypal() throws Throwable {
 		WebElement paypal = driver.findElement(By.cssSelector("#paypal_express"));
-		if (!driver.findElement(SignupObject.paypay_radio_button).isSelected())
+		/*if (!driver.findElement(SignupObject.paypay_radio_button).isSelected())
 		// to check the check box is already selected or not
 		{
 			driver.findElement(By.cssSelector("#paypal_express")).click();
 			//wait.implictywait(driver);
 			Thread.sleep(1000);
-		}
-
+		}*/
+                Thread.sleep(2000);
 		String paypal_text = driver.findElement(SignupObject.verify_paypal_text).getText();
 		String ExpectTitle = "You will be redirected to the PayPal website when you place an order.";
 		Assert.assertEquals(ExpectTitle, paypal_text);
