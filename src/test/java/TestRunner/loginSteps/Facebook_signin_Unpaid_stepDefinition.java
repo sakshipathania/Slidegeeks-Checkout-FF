16d7282d025fb85a58fa98460efee5aa2866b140
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-05-05 17:37:17 +0530

    Update Facebook_signin_Unpaid_stepDefinition.java

--- src/test/java/TestRunner/loginSteps/Facebook_signin_Unpaid_stepDefinition.java
+++ src/test/java/TestRunner/loginSteps/Facebook_signin_Unpaid_stepDefinition.java
@@ -58,10 +58,10 @@ public class Facebook_signin_Unpaid_stepDefinition extends SetupClass {
 	 driver.get("https://www.slideteam.net/complete-powerpoint-decks-presentations/all-powerpoint-complete-decks.html");
 	 Thread.sleep(3000);
 	 
-	 WebElement select_product= driver.findElement(By.cssSelector("li.product:nth-child(1) > div:nth-child(1) > div:nth-child(2) > strong:nth-child(1) > span:nth-child(1) > a:nth-child(1)"));
-	  Thread.sleep(2000);   
-	 select_product.click();
-	   Thread.sleep(2000);
+	// WebElement select_product= driver.findElement(By.cssSelector("li.product:nth-child(1) > div:nth-child(1) > div:nth-child(2) > strong:nth-child(1) > span:nth-child(1) > a:nth-child(1)"));
+	  //Thread.sleep(2000);   
+	 //select_product.click();
+	   //Thread.sleep(2000);
 	    
             }
 
@@ -162,7 +162,7 @@ public class Facebook_signin_Unpaid_stepDefinition extends SetupClass {
 		Thread.sleep(1000);
 	}
 
-	@Then("^Subscribe the product\\.$")
+	/*@Then("^Subscribe the product\\.$")
 	public void select_any_of_price_subscription() throws Throwable {
 		WebElement join_now_btn  = driver.findElement(By.xpath("//div[3]//div[3]//span[1]//form[1]//button[1]//span[1]"));
 			Thread.sleep(2000);
@@ -243,7 +243,48 @@ public class Facebook_signin_Unpaid_stepDefinition extends SetupClass {
 				  
 				  driver.switchTo().window(currentWindow);
 		    
+	} */
+	@Then("^user go to free ppts page fbu$")
+	public void user_go_to_free_ppts_page_efu() throws InterruptedException  {
+		Thread.sleep(2000);
+
+		WebElement free_ppt_btn=driver.findElement(By.xpath("//a[contains(.,'Free PPTs')]"));
+		Thread.sleep(2000);
+
+	    free_ppt_btn.click();
 	}
+
+	@Then("^user download a free ppt fbu$")
+	public void user_download_a_free_ppt_fbu() throws InterruptedException  {
+	    
+		//driver.findElement(By.xpath("//a[contains(.,'3d Puzzle Pieces In Line Powerpoint Presentation …')]")).click();
+		driver.get("https://www.slideteam.net/2d-pyramid-simple-powerpoint-presentation-slides.html");
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector("#clicking")).click();
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		Thread.sleep(2300);
+		
+	}
+
+	@Then("^user logout fbu\\.$")
+	public void user_logout_fbu() throws Throwable {
+	   Thread.sleep(8000);
+	        
+	    WebElement my_account =driver.findElement(By.cssSelector("body > div.page-wrapper > header > div.header.content > div.panel.wrapper > div > div > ul > li:nth-child(1) > a"));
+	    Thread.sleep(4000);
+	   my_account.click();
+	    Thread.sleep(4000);
+	    
+	
+		 Thread.sleep(3000);
+		 WebElement sign_out = driver.findElement(By.cssSelector("body > div.page-wrapper > header > div.header.content > div.panel.wrapper > div > div > ul > li.authorization-link > a"));
+		 Thread.sleep(3000);
+		 sign_out.click();
+		 Thread.sleep(3000);
+		
+	}
+	
 
 
 
