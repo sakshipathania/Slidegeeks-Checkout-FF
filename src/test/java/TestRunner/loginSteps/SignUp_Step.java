20ca7db6cd397499dda4f3265698002b41b62147
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-05-07 18:43:00 +0530

    Update SignUp_Step.java

--- src/test/java/TestRunner/loginSteps/SignUp_Step.java
+++ src/test/java/TestRunner/loginSteps/SignUp_Step.java
@@ -117,13 +117,12 @@ public class SignUp_Step extends SetupClass {
 		
 
 		Thread.sleep(2000);
-		try {
+		
 	   WebElement Email_Address = driver.findElement(By.xpath("//*[@id='email_address']"));
 		Thread.sleep(2000);
 	    Email_Address.sendKeys(full_email);
 		Thread.sleep(2000);
-	} catch (NoSuchElementException popup) {
-		}
+
 	}
 
 	@Then("^Enter First Name as \"([^\"]*)\"\\.$")
