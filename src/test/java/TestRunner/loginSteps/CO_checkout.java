a1e2867cf0a46331f84afd2bafa016f40229520b
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-06-11 10:56:29 +0530

    Update CO_checkout.java

--- src/test/java/TestRunner/loginSteps/CO_checkout.java
+++ src/test/java/TestRunner/loginSteps/CO_checkout.java
@@ -153,9 +153,9 @@ public class CO_checkout extends SetupClass{
 				    new_btn_signup.click();
 					Thread.sleep(2000);
 	}
-	
-	@Then("^Alert$")
-	public void Alert throws throwable {
+
+	@Then("^user is redirected to pricing page and choose a plan to pay (\\d+)CO$")
+	public void user_is_redirected_to_pricing_page_and_choose_a_plan_to_pay_CO(int arg1) throws Throwable {
 		Alert simpleAlert = driver.switchTo().alert();
 		{
 		String Monthly = driver.findElement(By.xpath("//*[@id='maincontent']/div[2]/div/div[1]/div[3]/div/div/div/div[2]/div[1]/span/span/em")).getText( );
@@ -175,12 +175,7 @@ public class CO_checkout extends SetupClass{
 		Thread.sleep(2000);
 		}
 		simpleAlert.accept();
-	}
-
-
-
-	@Then("^user is redirected to pricing page and choose a plan to pay (\\d+)CO$")
-	public void user_is_redirected_to_pricing_page_and_choose_a_plan_to_pay_CO(int arg1) throws Throwable {
+	
 		//assertEquals(driver.findElement(By.xpath("//*[@id='maincontent']/div[2]/div/div[1]/div[3]/div/div/div/div[2]/div[1]/span/span/em".getText(),"Monthly")));
 		// choose a plan
 		js.executeScript("window.scrollBy(0,1000)");
