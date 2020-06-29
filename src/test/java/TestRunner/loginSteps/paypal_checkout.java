ee2e79af702e1d31d1338e645736287d588ba425
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-06-29 17:23:32 +0530

    Update paypal_checkout.java

--- src/test/java/TestRunner/loginSteps/paypal_checkout.java
+++ src/test/java/TestRunner/loginSteps/paypal_checkout.java
@@ -534,14 +534,15 @@ public class paypal_checkout extends SetupClass {
 		    System.out.println("Title of the Page is --> "+pp_page_title);
 		    
 		 // place order button 
-		try{
+		/*try{
 			 WebElement cancel_order_btn  = driver.findElement(By.xpath("(//a[contains(.,'Cancel and return to Slideteam Pte. Ltd.')])[2]"));
 		js.executeScript("arguments[0].scrollIntoView();",cancel_order_btn);		
 		Thread.sleep(2000);
 			    cancel_order_btn.click();
 				Thread.sleep(5000);
 		} catch(NoSuchElementException popup) {
-		}
+		} */
+		
 
 				 // Switch To Default Window
 				  
