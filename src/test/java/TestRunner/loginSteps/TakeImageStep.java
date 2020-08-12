f8d6c40769d936dde44446ba22ef7b126b470016
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-08-12 16:56:12 +0530

    Update TakeImageStep.java

--- src/test/java/TestRunner/loginSteps/TakeImageStep.java
+++ src/test/java/TestRunner/loginSteps/TakeImageStep.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.apache.commons.io.FileUtils;
 import java.io.IOException;
+import java.io.file;
 
 
 import TestRunner.SetupClass;
