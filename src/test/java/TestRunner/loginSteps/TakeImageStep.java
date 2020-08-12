6bdd9ccb2b54a1d6c1f6f3b23a9b6c649dc7c994
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-08-12 16:15:12 +0530

    Update TakeImageStep.java

--- src/test/java/TestRunner/loginSteps/TakeImageStep.java
+++ src/test/java/TestRunner/loginSteps/TakeImageStep.java
@@ -2,6 +2,9 @@ package TestRunner.loginSteps;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.apache.commons.io.FileUtils;
+import java.io.IOException;
+
 
 import TestRunner.SetupClass;
 import cucumber.api.Scenario;
@@ -15,6 +18,15 @@ import cucumber.api.java.After;
 		      log.info("Scenario failed, now taking screenshot");
 		      final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 		      scenario.embed(screenshot, "image/png");
+			  
+			    try {
+				    FileUtils.copyFile(src, new File("C:\Failed test cases/error.png"));
+				}
+			    catch (IOException e)
+                               {
+                            System.out.println(e.getMessage());
+ 
+                             }
 		    }
 		}
 	}
