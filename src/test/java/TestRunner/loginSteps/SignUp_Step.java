37e000a165090a6fb51774f38b0b5b8f9c2b432d
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-09-29 16:08:36 +0530

    Update SignUp_Step.java

--- src/test/java/TestRunner/loginSteps/SignUp_Step.java
+++ src/test/java/TestRunner/loginSteps/SignUp_Step.java
@@ -233,7 +233,8 @@ public class SignUp_Step extends SetupClass {
 	@Then("^user wants to delete the new account created cd$")
 	public void user_wants_to_delete_the_new_account_created_cd() throws InterruptedException  {
 
-		 driver.findElement(By.xpath("//a[contains(text(),'My Account')]")).click();
+		// driver.findElement(By.xpath("//a[contains(text(),'My Account')]")).click();
+		driver.get("https://www.slideteam.net/customer/account/");
 		 Thread.sleep(3000);
 		 
 
