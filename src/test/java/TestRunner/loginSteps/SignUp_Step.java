1b523f9f3efa1f8c624259bfc4e0dedc9127b20b
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-10-22 16:00:09 +0530

    Update SignUp_Step.java

--- src/test/java/TestRunner/loginSteps/SignUp_Step.java
+++ src/test/java/TestRunner/loginSteps/SignUp_Step.java
@@ -1,4 +1,4 @@
-package TestRunner.loginSteps;
+/*package TestRunner.loginSteps;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -193,17 +193,17 @@ public class SignUp_Step extends SetupClass {
 
 	
 	@Then("^After signup see the price page$")
-	public void After_signup_see_the_price_page() throws InterruptedException {
-		/*driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		String actualTitle = driver.getTitle();
-		//wait.implictywait(driver);
-		Thread.sleep(2000);
-		String expectedTitle = "Pricing";
-		//wait.implictywait(driver);
-		Thread.sleep(2000);
-		Assert.assertEquals(expectedTitle, actualTitle);
-		//wait.implictywait(driver);*/
-		driver.get("https://www.slideteam.net/pricing?utm_expid=.ob6CI18MTrailD_9AQRC-g.0&utm_referrer=https%3A%2F%2Fwww.slideteam.net%2Fpricing");
+	public void After_signup_see_the_price_page() throws InterruptedException {*/
+		                                    /*driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		                                        String actualTitle = driver.getTitle();
+		                                       //wait.implictywait(driver);
+		                                          Thread.sleep(2000);
+		                                          String expectedTitle = "Pricing";
+		                                            //wait.implictywait(driver);
+		                                                Thread.sleep(2000);
+		                                                  Assert.assertEquals(expectedTitle, actualTitle);
+		                                                        //wait.implictywait(driver);*/
+		/*driver.get("https://www.slideteam.net/pricing?utm_expid=.ob6CI18MTrailD_9AQRC-g.0&utm_referrer=https%3A%2F%2Fwww.slideteam.net%2Fpricing");
 		Thread.sleep(2000);
 		Actions actions = new Actions(driver);
 		WebElement free_ppt_btn1=wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Free Stuff")));
@@ -281,4 +281,4 @@ public class SignUp_Step extends SetupClass {
 		 }
 	
       }
-}
+}*/
