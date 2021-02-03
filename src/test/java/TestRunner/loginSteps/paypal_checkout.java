d16198a6ec358119e32c2ca32014414a851e381f
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-02-03 14:44:59 +0530

    Update paypal_checkout.java

--- src/test/java/TestRunner/loginSteps/paypal_checkout.java
+++ src/test/java/TestRunner/loginSteps/paypal_checkout.java
@@ -3,7 +3,7 @@ package TestRunner.loginSteps;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import org.junit.Assert;
-import java.lang.*;
+java.lang.String.isEmpty();
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
