fbf173e52d1bec9a28408bbe7b3357bdbbe0ed95
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2022-01-06 10:58:35 +0530

    Update SignUp_Step.java

--- src/test/java/TestRunner/loginSteps/SignUp_Step.java
+++ src/test/java/TestRunner/loginSteps/SignUp_Step.java
@@ -197,12 +197,12 @@ public class SignUp_Step extends SetupClass {
 		Thread.sleep(3000);
 		No_Delete.click();
 		Thread.sleep(7000);
-		String verifyDeleteAccountMessage = wait
+		/*String verifyDeleteAccountMessage = wait
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='alert-message login-sucesmsg']")))
 				.getText();
 		System.out.println("verifyText1 = " + verifyDeleteAccountMessage);
 
-		Assert.assertTrue("Your are not on paypal page", verifyDeleteAccountMessage.contentEquals("Your Account has been deleted successfully."));
+		Assert.assertTrue("Your are not on paypal page", verifyDeleteAccountMessage.contentEquals("Your Account has been deleted successfully."));*/
                 
      //  WebElement Signout = driver.findElement(By.xpath("//a[@href ='/logout']"));
 	//	Thread.sleep(3000);
