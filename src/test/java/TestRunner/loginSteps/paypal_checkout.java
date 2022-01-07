dc337faaab9d4f6734c7e9528f8e64d37baf0185
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2022-01-07 11:13:08 +0530

    Update paypal_checkout.java

--- src/test/java/TestRunner/loginSteps/paypal_checkout.java
+++ src/test/java/TestRunner/loginSteps/paypal_checkout.java
@@ -825,13 +825,13 @@ public class paypal_checkout extends SetupClass {
 	WebElement No_Delete = driver.findElement(By.xpath("/html/body/div[1]/div[5]/div/div/div[3]/button[2]"));
 		Thread.sleep(2000);
 		No_Delete.click();
-                 Thread.sleep(3000);
-		
+                 Thread.sleep(7000);
+		  Thread.sleep(7000);
 		String verifyDeleteAccountMessage = wait
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='alert-message login-sucesmsg']")))
 				.getText();
 		System.out.println("verifyText1 = " + verifyDeleteAccountMessage);
-
+                   Thread.sleep(3000);
 		Assert.assertTrue("Your account is not deleted", verifyDeleteAccountMessage.contentEquals("Your Account has been deleted successfully."));
      //  WebElement Signout = driver.findElement(By.xpath("//a[@href ='/logout']"));
 	//	Thread.sleep(3000);
