1d3a0e3aab10191c191e0ea7144059869188bdf4
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2022-01-11 10:37:50 +0530

    Update Coupon.java

--- src/test/java/TestRunner/loginSteps/Coupon.java
+++ src/test/java/TestRunner/loginSteps/Coupon.java
@@ -299,8 +299,8 @@ public class Coupon extends SetupClass {
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='alert-message login-sucesmsg']")))
 				.getText();
 		System.out.println("verifyText1 = " + verifyDeleteAccountMessage);
-                 Thread.sleep(3000);*/
-		Assert.assertTrue("Your are not on paypal page", verifyDeleteAccountMessage.contentEquals("Your Account has been deleted successfully."));
+                 Thread.sleep(3000);
+		Assert.assertTrue("Your are not on paypal page", verifyDeleteAccountMessage.contentEquals("Your Account has been deleted successfully."));*/
      //  WebElement Signout = driver.findElement(By.xpath("//a[@href ='/logout']"));
 	//	Thread.sleep(3000);
 		//Signout.click();
