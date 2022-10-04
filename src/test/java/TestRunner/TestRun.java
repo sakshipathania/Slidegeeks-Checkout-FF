a87d042770ab5761aece002176571f84d93213d7
nishaslidetech <nisha.dhiman@slidetech.in>
2022-10-04 18:12:23 +0530

    first commit

--- src/test/java/TestRunner/TestRun.java
+++ src/test/java/TestRunner/TestRun.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "." },  tags = ("@Coupon"),
+@CucumberOptions(features = { "." },  //tags = ("@Coupon"),
 		plugin = { "pretty", "html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json",
 				"usage:target/usage.jsonx", "junit:target/cucumber.xml" }// ,
 //monochrome = true, //display the console output in a proper readable format
